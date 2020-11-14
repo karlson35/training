41e64c61e25f13e7fd36b2b360bda69a7f27cf42
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-14 16:28:32 +0300

    lambda

--- geometry.py
+++ geometry.py
@@ -1,6 +1,12 @@
 from geom2d import *
 
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-l2 = sorted(l1)
-print(l1)
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
+
+l2 = list(map(lambda p: Point(p.x, -p.y), l))
+
+l2 = list(filter(lambda p: p.x > 0, l))
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
+
+
+print(l)
 print(l2)
