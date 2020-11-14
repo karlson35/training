ebc385e43dac7e26441851e3c68f285735a31f07
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-14 16:06:15 +0300

    1st Commit

---
+++ geometry.py
@@ -0,0 +1,6 @@
+from geom2d import *
+
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l2 = sorted(l1)
+print(l1)
+print(l2)
---
+++ hello.py
@@ -0,0 +1,8 @@
+
+def hello(s):
+    print("Hello, " + s + "!")
+
+
+hello("world")
+hello("python")
+hello("Igor")
